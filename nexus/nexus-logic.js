25e2028b32d429221910749365fabfe8bbcc44e6
Magoso <70547408+GrampyBear@users.noreply.github.com>
2026-05-05 17:07:43 -0400

    Update nexus-logic.js

--- nexus/nexus-logic.js
+++ nexus/nexus-logic.js
@@ -1,1 +1,16 @@
+const cards = document.querySelectorAll('.card');
+const body = document.body;
 
+cards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+        if (card.classList.contains('my-work')) {
+            body.style.backgroundColor = "#0a121a"; // Tono frío
+        } else {
+            body.style.backgroundColor = "#1a0a0a"; // Tono cálido
+        }
+    });
+
+    card.addEventListener('mouseleave', () => {
+        body.style.backgroundColor = "#0d0d0d"; // Volver al neutro
+    });
+});
